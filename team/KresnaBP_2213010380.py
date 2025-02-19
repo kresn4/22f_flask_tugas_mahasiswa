9d246919e27f872c22f2b478af58c643b47c03a2
bittersweet <129123413+kresn4@users.noreply.github.com>
2025-02-20 04:29:57 +0700

    Create KresnaBP_2213010380.py

---
+++ team/KresnaBP_2213010380.py
@@ -0,0 +1,10 @@
+def perkenalan (nama, nim, peran):
+    print ("Perkenalkan, Saya ", nama)
+    print ("NIM : ", nim)
+    print ("Peran saya yaitu : ", peran)
+    
+if __name__ == "__main__":
+    nama = "Kresna Bayu Prasetyo"
+    nim = "2213010380"
+    peran = "Programmer"
+    perkenalan(nama, nim, peran)
